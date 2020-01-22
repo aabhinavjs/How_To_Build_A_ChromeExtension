1db3850f9c367faa75c984a5aab80f708ee70709
Abhinav Anand <abhinav.codeinfo@gmail.com>
2020-01-22 15:15:30 +0530

    Adding Content.js
    Showing An alert when user clicks
    on extension Icon

---
+++ demoextension/extension_bundle/content.js
@@ -0,0 +1,8 @@
+alert("Generating Images Map Of This Site. This may take up to 30 seconds depending on site");
+debugger;
+function unicodeToChar(text) {
+	return text.replace(/\\u[\dA-F]{4}/gi, 
+	      function (match) {
+	           return String.fromCharCode(parseInt(match.replace(/\\u/g, ''), 16));
+	      });
+}
