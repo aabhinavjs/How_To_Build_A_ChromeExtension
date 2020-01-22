feb95ed4115cb6dcc0b24e60ccc44930e1936492
Abhinav Anand <abhinav.codeinfo@gmail.com>
2020-01-22 15:23:59 +0530

    Modified Content.js
    wrote a function to grab all the images
    present on the site

--- demoextension/extension_bundle/content.js
+++ demoextension/extension_bundle/content.js
@@ -6,3 +6,23 @@ function unicodeToChar(text) {
 	           return String.fromCharCode(parseInt(match.replace(/\\u/g, ''), 16));
 	      });
 }
+
+// called the collect_images function to get html generated which contains images.
+
+item_embedded_in_body = collect_images(document.images)
+document.body.innerHTML = item_embedded_in_body;
+
+
+// written function to collect images of the site.
+// uses concepts of Array collect site images into it by grabbing it from web document
+
+function collect_images(images){
+	debugger;
+	imagestrings = ''
+	image_elements = Array.from(images)
+	$.each(image_elements,function(key,value){
+		imagestrings += '<div class="class=" carousel carousel-item""><img class="mr-100" src="'+value.src+'"" alt="Italian Trulli"></div>'
+	})
+
+	return imagestrings
+}
